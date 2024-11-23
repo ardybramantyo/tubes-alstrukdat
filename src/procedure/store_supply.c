7b987b57dab74a5b347659f3640a910a6395bffc
SArdyBramantyo <168545215+SArdyBramantyo@users.noreply.github.com>
2024-11-23 14:42:29 +0700

    Update store_supply.c
    Menyesuaikan ADT

--- src/procedure/store_supply.c
+++ src/procedure/store_supply.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "boolean.h"
-#include "circular_queue.h"
+#include "Queue.h"
+#include "ArrayDin.h"
+#include "Barang.h"
+#include "MesinKata.h"
 
 int Uppercased_Char(int num){
     if (num >= 'a' && num <= 'z'){
@@ -23,142 +27,54 @@ boolean is_same_string(const char str1[], const char str2[]){
     return (str1[i] == '\0' && str2[i] == '\0');
 }
 
-void StoreSupply(){
+/*----------------------------------------------------DIVIDER-----------------------------------------------------------*/
+
+void StoreSupply(ArrayDin arr){
     
     if (!isEmpty(requestQueue)){
-        char input[51], c_input;
-        int i = 0;
-
-        ElType val;
-        dequeue(&requestQueue, &val);
+        char* queue_front = front(requestQueue);
         
-        printf("Apakah kamu ingin menambahkan barang %s:", val);
-        while ((c_input = getc(stdin)) != '\n' && i < 50){
-            input[i] = c_input;
-            i++;
-        }
-        input[i] ='\0';
+        printf("Apakah kamu ingin menambahkan barang %s: ", queue_front);
+        
+        startKata(NULL);
+
+        char input[currentKata.length+1];
+        strcpy(input, currentKata.buffer);
+
+        /*----------------------------------------------------DIVIDER-----------------------------------------------------------*/
 
         if (is_same_string(input, "Terima")){
-            i = 0;
-            char inputHarga_String[11], c_inputHarga;
+            char* val = dequeue(&requestQueue);
+            int inputHarga = 0;
 
-            while ((c_inputHarga = getc(stdin)) != '\n' && i < 11){
-                inputHarga_String[i] = c_inputHarga;
-                i++;
-            }
-            inputHarga_String[i] = '\0';
-            int inputHarga = atoi(inputHarga_String);
-
+            while (inputHarga <= 0){
+                printf("Harga barang:");
+                inputHarga = getNum();
 
                 if (inputHarga <= 0){
-                    printf("Harga barang tidak valid, silahkan mengulang kembali!");
+                    printf("Harga barang tidak valid, silahkan mengulang kembali!\n");
                 }
-
-                else{   
-                    /*LANJUTKAN NANTI .... --> Pengen di loop untuk nerima getc(stdin), tapi untuk sekarang ini dlu :D, biar bisa mulai*/
-                }
-
-            /*------------------------------------------------------DIVIDER--------------------------------------------------------*/
-            FILE* store_data;
-            store_data = fopen("inventory.txt", "r");
-
-            int N_Store;
-            i = 0;
-            char N_Store_String[11], c;
-        
-            while ( (c = getc(store_data)) != '\n'  &&  c != EOF  &&  i < 11){
-                N_Store_String[i] = c;
-                i++;
-            }
-            N_Store_String[i] = '\0';
-            N_Store = atoi(N_Store_String);
+            }      
 
             /*------------------------------------------------------DIVIDER--------------------------------------------------------*/
 
-
-            FILE* store_data_temp;
-            store_data_temp = fopen("temp.txt", "w");
-
-            char arrTxt[1024];
-            int c_arrTxt;
-            i = 0;
-            while ( (c_arrTxt = getc(store_data)) != EOF){
-                arrTxt[i] = c_arrTxt;
-                i++; 
-            }
-            arrTxt[i] = '\0';
-            
-            closef(store_data);
+            ArrayDinInsertEnd(arr, ); /*Cara Masukin Harga dan Nama Gimana Ya....*/
+        }
 
             /*------------------------------------------------------DIVIDER--------------------------------------------------------*/
-
-            
-            int line_number = 0, index = 0;
-            while (arrTxt[index] != '\0'){
-                line_number++;
-
-                int index_line_first = index;
-                while (arrTxt[index] != '\n' && arrTxt[index] != '\0'){
-                    index++;
-                }
-                int index_line_last = index;
-
-                if (arrTxt[index] == '\n'){
-                    index++;
-                }
-
-                /*------------------------------------------------------DIVIDER--------------------------------------------------------*/
-
-
-                char arrTxt_line[1024];
-                int index_line_length = index_line_last - index_line_first;
-                for (int j = 0; j < (index_line_length); j++){
-                    arrTxt_line[j] = arrTxt[index_line_first + j];
-                }
-                arrTxt_line[index_line_length] = '\0';
-
-                /*------------------------------------------------------DIVIDER--------------------------------------------------------*/
-
-                
-                if (line_number == 1){
-                    fprintf(store_data_temp, "%d\n", (N_Store + 1));
-                    fprintf(store_data_temp, "%s\n", arrTxt_line);
-                }
-
-                else if (line_number == N_Store){
-                    fprintf(store_data_temp, "%s\n", arrTxt_line);
-                    fprintf(store_data_temp, "%d %s\n", inputHarga, val);
-                }
-
-                else{
-                    fprintf(store_data_temp,"%s\n", arrTxt_line);
-                }
-            }
-
-            closef(store_data_temp);
-
-            remove("inventory.txt");
-            rename("temp.txt", "inventory.txt");  
-            }
-
-
-            /*------------------------------------------------------DIVIDER--------------------------------------------------------*/
-
-
 
         else if (is_same_string(input, "Tunda")){
+            char* val = dequeue(&requestQueue);
             enqueue(&requestQueue, val);
             printf("%s dikembalikan ke antrean", val);
         }
 
         else if (is_same_string(input, "Tolak")){
-
+            char* val = dequeue(&requestQueue);
         }
 
         else{
-
+            return;
         }
     }
-
 }
